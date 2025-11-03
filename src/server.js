d8267ff847a0d9487c5343caf5c41fdba2326737
Rashed <rashedalamin69@gmail.com>
2025-11-04 01:17:21 +0600

    first to demo branch

--- src/server.js
+++ src/server.js
@@ -1,2 +1,4 @@
 const {ApolloServer}=require('@apollo/server')
 const {startStandaloneServer}=require('@apollo/server/standalone')
+const typeDefs=require('./graphql/schema')
+const resolvers=require('./graphql/resolvers')
